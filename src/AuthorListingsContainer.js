3f31894849974403276b86f642290df3a0b35bcd
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-03-01 16:16:20 -0500

    tempoarly fixing authors page from crashing

--- src/AuthorListingsContainer.js
+++ src/AuthorListingsContainer.js
@@ -6,7 +6,7 @@ export default function AuthorListingsContainer({authorIdList})  {
     
   return (
     <div className='ui cards'>
-        {authorIdList.books.map((book) => <AuthorBookListingCard book={book} />)}
+        {/* {authorIdList.books.map((book) => <AuthorBookListingCard book={book} />)} */}
     </div>
   )
 }
