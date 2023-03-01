c8484bcf5600692544d4770c7857caf9d7a53c33
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-03-01 17:31:54 -0500

    adding to update git

--- src/BookListingsContainer.js
+++ src/BookListingsContainer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import BookListingCard from './BookListingCard'
 
-export default function BookListingsContainer({bookList})  {
+export default function BookListingsContainer({bookList, deleteCallback})  {
 
   return (
     <div className='ui cards'>
-        {bookList.map((book) => <BookListingCard book={book} />)}
+        {bookList.map((book) => <BookListingCard book={book} deleteCallback={deleteCallback}/>)}
     </div>
   )
 }
